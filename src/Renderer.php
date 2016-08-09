71ea1fc498c00c9ebe04cbed0d3d107a4ffd3cf9
Dmytro Sokil <dmytro.sokil@gmail.com>
2016-08-09 19:03:45 +0300

    allow customize markup

--- src/Renderer.php
+++ src/Renderer.php
@@ -8,9 +8,96 @@ class Renderer
 {
     private $differ;
 
-    public function __construct()
+    const FORMAT_DEFAULT    = 0;
+    const FORMAT_COLOUR     = 1;
+
+    private $predefinedFormats = [
+        self::FORMAT_DEFAULT => [
+            'insert' => [
+                'tag' => 'ins',
+            ],
+            'delete' => [
+                'tag' => 'del',
+            ]
+        ],
+        self::FORMAT_COLOUR => [
+            'insert' => [
+                'tag' => 'ins',
+                'attributes' => 'style="background: #ddfade;"',
+            ],
+            'delete' => [
+                'tag' => 'del',
+                'attributes' => 'style="background: #ffe7e7;"',
+            ]
+        ],
+    ];
+
+    /**
+     * @var array
+     */
+    private $tags;
+
+    public function __construct(array $options = [])
     {
         $this->differ = new Differ('', false);
+
+        // set format
+        if (!empty($options['format'])) {
+            $this->setFormat($options['format']);
+        } else {
+            $this->setFormat(self::FORMAT_DEFAULT);
+        }
+    }
+
+    private function normalizeFormat($format)
+    {
+        // format specified as numeric
+        if (is_numeric($format)) {
+            if (!isset($this->predefinedFormats[$format])) {
+                throw new \InvalidArgumentException('Invalid format specified');
+            }
+
+            return $this->predefinedFormats[$format];
+        }
+
+        if (!is_array($format)) {
+            throw new \InvalidArgumentException('Invalid format specified');
+        }
+
+        // format as array passed
+        $normalizedFormat = $this->predefinedFormats[self::FORMAT_DEFAULT];
+        foreach (['insert', 'delete'] as $formatType) {
+            foreach (['tag', 'attributes'] as $formatParameter) {
+                if (!empty($format[$formatType][$formatParameter])) {
+                    $normalizedFormat[$formatType][$formatParameter] = $format[$formatType][$formatParameter];
+                }
+            }
+        }
+
+        return $normalizedFormat;
+    }
+
+    public function setFormat($format)
+    {
+        $format = $this->normalizeFormat($format);
+
+        $this->tags['insertOpenTag'] = sprintf(
+            '<%s%s>',
+            $format['insert']['tag'],
+            isset($format['insert']['attributes']) ? ' ' . $format['insert']['attributes'] : null
+        );
+
+        $this->tags['insertClosedTag'] = '</' . $format['insert']['tag'] . '>';
+
+        $this->tags['deleteOpenTag'] = sprintf(
+            '<%s%s>',
+            $format['delete']['tag'],
+            isset($format['delete']['attributes']) ? ' ' . $format['delete']['attributes'] : null
+        );
+
+        $this->tags['deleteClosedTag'] = '</' . $format['delete']['tag'] . '>';
+
+        return $this;
     }
 
     /**
@@ -20,22 +107,25 @@ class Renderer
      */
     public function render($from, $to)
     {
+        // get diff
         $diff = $this->differ->diffToArray($from, $to);
 
+        // render diff
         foreach ($diff as &$line) {
             switch ($line[1]) {
                 case 0: // NOT CHANGED
                     $line = $line[0];
                     break;
                 case 1: // ADDED
-                    $line = '<ins>' . $line[0] . '</ins>';
+                    $line = $this->tags['insertOpenTag'] . $line[0] . $this->tags['insertClosedTag'];
                     break;
                 case 2: // REMOVED
-                    $line = '<del>' . $line[0] . '</del>';
+                    $line = $this->tags['deleteOpenTag'] . $line[0] . $this->tags['deleteClosedTag'];
                     break;
             }
         }
 
+        // build diff
         return implode(PHP_EOL, $diff);
     }
 }
--- tests/RendererTest.php
+++ tests/RendererTest.php
@@ -21,4 +21,80 @@ class RendererTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expectedDiff, $actualDiff);
     }
+
+    public function testRender_CustomFullySpecifiedFormat()
+    {
+        $expectedDiff = implode(PHP_EOL, [
+            'line1',
+            '<del style="background: #ffe7e7;">line2</del>',
+            '<ins style="background: #ddfade;">line2changed</ins>',
+            'line3'
+        ]);
+
+        $diffRenderer = new Renderer([
+            'format' => [
+                'insert' => [
+                    'tag' => 'ins',
+                    'attributes' => 'style="background: #ddfade;"',
+                ],
+                'delete' => [
+                    'tag' => 'del',
+                    'attributes' => 'style="background: #ffe7e7;"',
+                ]
+            ]
+        ]);
+
+        $actualDiff = $diffRenderer->render(
+            implode(PHP_EOL, ['line1', 'line2', 'line3']),
+            implode(PHP_EOL, ['line1', 'line2changed', 'line3'])
+        );
+
+        $this->assertEquals($expectedDiff, $actualDiff);
+    }
+
+    public function testRender_CustomPartlySpecifiedFormat()
+    {
+        $expectedDiff = implode(PHP_EOL, [
+            'line1',
+            '<del>line2</del>',
+            '<ins class="insert">line2changed</ins>',
+            'line3'
+        ]);
+
+        $diffRenderer = new Renderer([
+            'format' => [
+                'insert' => [
+                    'attributes' => 'class="insert"',
+                ],
+            ]
+        ]);
+
+        $actualDiff = $diffRenderer->render(
+            implode(PHP_EOL, ['line1', 'line2', 'line3']),
+            implode(PHP_EOL, ['line1', 'line2changed', 'line3'])
+        );
+
+        $this->assertEquals($expectedDiff, $actualDiff);
+    }
+
+    public function testRender_CustomNumericFormat()
+    {
+        $expectedDiff = implode(PHP_EOL, [
+            'line1',
+            '<del style="background: #ffe7e7;">line2</del>',
+            '<ins style="background: #ddfade;">line2changed</ins>',
+            'line3'
+        ]);
+
+        $diffRenderer = new Renderer([
+            'format' => Renderer::FORMAT_COLOUR
+        ]);
+
+        $actualDiff = $diffRenderer->render(
+            implode(PHP_EOL, ['line1', 'line2', 'line3']),
+            implode(PHP_EOL, ['line1', 'line2changed', 'line3'])
+        );
+
+        $this->assertEquals($expectedDiff, $actualDiff);
+    }
 }
